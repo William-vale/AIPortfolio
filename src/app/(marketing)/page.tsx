4d7efc0ce0cb898796a1c5f2746a94e8c2156c3b
William do Vale <63527052+William-vale@users.noreply.github.com>
2026-08-21 20:39:36 +0000

    construção da primeira tela: landing-page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,28 +1,32 @@
-'use client - uso do cliente'
+'use client'
 
-import { X, Globe2, ArrowRigth, Check, FolderKanban, UserRound } from 'lucide-react'
+import { ArrowRight, Check, FolderKanban, Globe2, Menu, Sparkles, UserRound, X } from 'lucide-react'
 import { useState } from 'react'
 
 const features = [
     {
         icon: UserRound,
         title: 'Como funciona',
-        text: 'Cadastre o suas informações profissionais uma vez e publique um endereço público para compartilhamento.',
+        text: 'Cadastre seu perfil uma vez e compartilhe um endereço público.'
     },
     {
         icon: FolderKanban,
         title: 'Para desenvolvedores',
-        text: 'Unifique experiências, projetos e habilidades em um só lugar.',
+        text: 'Unifique experiências, projetos e habilidades em um só lugar.'
     },
     {
         icon: Globe2,
         title: 'Para recrutadores',
-        text: 'Acesse o portfólio personalizado sem cadastro.',
+        text: 'Acesse o portfólio personalizado sem cadastro.'
     },
 ]
 
 export default function Home() {
-    const [menuOpen, SetMenuOpen] = useState(false)
+    const [menuOpen, SetMenuOpen] = useState(false);
+    return (
+        <main className="min-h-screen ">
 
-    
+        </main>
+    );
+
 }
