ae6acda49e975a5488731b69e7f68bb6b53cdb4b
William do Vale <63527052+William-vale@users.noreply.github.com>
2026-08-26 14:04:01 -0300

    Modify next.config.ts for static export settings
    Updated next.config.ts to include configuration options for static export and image optimization.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
-import type { NextConfig } from "next";
+/* import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {/* config options here };
+
+export default nextConfig; */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Força a geração de HTML/CSS/JS estáticos
+  images: {
+    unoptimized: true, // Desativa a otimização de imagem que exige servidor
+  },
+  // ATENÇÃO: Descomente as linhas abaixo APENAS se você NÃO estiver usando um domínio personalizado.
+  // basePath: '/nome-do-seu-repositorio',
+  // assetPrefix: '/nome-do-seu-repositorio/',
+};
 
 export default nextConfig;
