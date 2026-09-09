026854c949cfb913665b0b03291bbcf620af826d
William do Vale <63527052+William-vale@users.noreply.github.com>
2026-09-09 07:49:32 -0300

    Refactor proxy function to update session

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,12 @@
-import { NextResponse } from 'next/server'
+import { updateSession } from './lib/supabase/proxy'
 import type { NextRequest } from 'next/server'
 
-export function proxy(request: NextRequest) {
-    return NextResponse.redirect(new URL('/home', request.url))
+export async function proxy(request: NextRequest) { 
+    return await updateSession(request)
 }
 
 export const config = {
-    matcher: '/about/:path*',
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    ],
 }
